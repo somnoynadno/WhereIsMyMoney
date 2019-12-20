2b2e4e0e2aa45e0e4c976aa3ec133e94d18905a3
somnoynadno <somnoynadno@yandex.ru>
2019-12-20 09:53:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/whereismymoney/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.whereismymoney
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class sMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
+        setContentView(R.layout.activity_main)
+    }
+}
