d687c8e988a8e56fd530d4f80cb036ffd7f0bf8e
somnoynadno <somnoynadno@yandex.ru>
2019-12-21 16:40:15 +0700

    Date picker dialog

--- app/src/main/java/com/example/whereismymoney/NewDebtActivity.kt
+++ app/src/main/java/com/example/whereismymoney/NewDebtActivity.kt
@@ -8,9 +8,17 @@ import androidx.appcompat.widget.Toolbar
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.new_debt_layout.*
 import android.app.Activity
+import android.app.DatePickerDialog
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import java.util.*
+import java.text.SimpleDateFormat
 
 
 class NewDebtActivity : AppCompatActivity() {
+
+    private var chosenDate = ""
+    private var date = Date()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,12 +35,76 @@ class NewDebtActivity : AppCompatActivity() {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
 
-        saveButton.setOnClickListener{
-            // save action
-            val returnIntent = Intent()
-            returnIntent.putExtra("result", 1)
-            setResult(Activity.RESULT_OK, returnIntent)
-            finish()
+        datePickerButton.setOnClickListener {
+            alertDatePickerDialog()
         }
+
+        saveButton.setOnClickListener {
+            alertSaveDialog()
+        }
+    }
+
+
+    private fun alertSaveDialog() {
+        val builder = AlertDialog.Builder(this@NewDebtActivity)
+
+        builder.setMessage("Сохранить?")
+
+        builder.setPositiveButton("Да") { dialog, which ->
+            saveDebtAndExit()
+        }
+
+        builder.setNeutralButton("Отмена") { _, _ ->
+            Toast.makeText(
+                applicationContext, "Отмена",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+
+    private fun alertDatePickerDialog() {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        val dpd = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                chosenDate = dayOfMonth.toString() + "." +
+                        (monthOfYear + 1).toString() + "." +
+                        year.toString()
+
+                val toast = Toast.makeText(
+                    applicationContext,
+                    "Дата выбрана: " + chosenDate, Toast.LENGTH_SHORT
+                )
+                toast.show()
+
+                val pattern = "dd.MM.yyyy"
+                date = SimpleDateFormat(pattern).parse(chosenDate)
+                println(date)
+
+                dateText.text = "Вернёт до " + chosenDate
+            },
+            year,
+            month,
+            day
+        )
+
+        dpd.show()
+    }
+
+
+    private fun saveDebtAndExit(){
+        // TODO: save debt
+        val returnIntent = Intent()
+        returnIntent.putExtra("result", 1)
+        setResult(Activity.RESULT_OK, returnIntent)
+        finish()
     }
 }
